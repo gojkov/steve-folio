95f8dd64a37e96da9b8c673cfcc37cf4f8b16521
Steve Gojkov <stevegojkov@gmail.com>
2020-01-06 21:15:20 -0500

    new page commit

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+$("span").on("mouseover", function(){
+    $(this).css("color", "#d4af37");
+});
+
+$("span").on("mouseout", function(){
+    $(this).css("color", "ivory");
+});
---
+++ purgecss.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    content: ['./dist/index.html'],
+    css: ['./dist/style.css'],
+    whitelist: ['h1', 'body', 'ion-icon', 'img:hover', 'cinzel', 'footer', 'main', '@media']
+}
