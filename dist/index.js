27a91e1d1d7247f7d773d105ef52330ffac1896f
Steve Gojkov <stevegojkov@gmail.com>
2020-01-20 19:41:36 -0500

    background image now always 100vh

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,1 @@
-$("span").on("mouseover", function(){
-    $(this).css("color", "#d4af37");
-});
-
-$("span").on("mouseout", function(){
-    $(this).css("color", "ivory");
-});
+$("span").on("mouseover",function(){$(this).css("color","#d4af37")});$("span").on("mouseout",function(){$(this).css("color","ivory")});
